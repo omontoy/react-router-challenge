87b66976046e58a413cf7b86c7f8113cd98c6c6e
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-04-04 16:33:15 -0500

    Add Newsletter and NewsletterSignup components to extends functionalities

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import HomePage from "./pages/Home";
 import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 function App() {
   const myRoutes = createBrowserRouter([
@@ -74,6 +75,11 @@ function App() {
               action: manipulateEventAction,
             },
           ],
+        },
+        {
+          path: "newsletter",
+          element: <NewsletterPage />,
+          action: newsletterAction,
         },
       ],
     },
